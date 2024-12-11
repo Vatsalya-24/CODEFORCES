33c44793bd64e6c3f7670ed591ed15ccfd19a389
vatsalya <vatsalyasharma24@gmail.com>
2024-12-11 22:55:33 +0530

    Petya_Strings

--- Petya_and_Strings.java
+++ Petya_and_Strings.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class Petya_and_Strings {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the String one:");
+        String first = input.nextLine();
+
+        System.out.println("Enter the String two:");
+        String two = input.nextLine();
+
+        first = first.toLowerCase();
+        two = two.toLowerCase();
+
+        int comparison = first.compareTo(two);
+
+        if (comparison < 0) {
+            System.out.println("-1");
+        } else if (comparison > 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+
+        input.close();
+    }
 }
